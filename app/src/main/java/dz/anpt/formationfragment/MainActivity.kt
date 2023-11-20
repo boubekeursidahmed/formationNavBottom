58be879610c59ffd78e535acc5b43074a84f2a89
sid ahmed <sidou226@gmail.com>
2023-11-20 13:36:01 +0100

    init

---
+++ app/src/main/java/dz/anpt/formationfragment/MainActivity.kt
@@ -0,0 +1,26 @@
+package dz.anpt.formationfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottonVavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        setupWithNavController(bottonVavigationView, navController)
+
+    }
+}
